4f8870b22a648f0432972499e7c0626a83fd7828
NDIONGUE <112211427+MohamedLammineNDIONGUE1601@users.noreply.github.com>
2024-12-22 11:24:33 +0100

    md5.h

--- src/backend/hash/md5.h
+++ src/backend/hash/md5.h
@@ -1,1 +1,25 @@
 
+#ifndef MD5_H
+#define MD5_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+#define MD5_BLOCK_SIZE 16       // MD5 outputs a 16 byte digest
+
+typedef unsigned char BYTE;     // 8-bit byte
+typedef unsigned int WORD;       // 32-bit word, change to "long" for 16-bit machines
+
+typedef struct {
+    BYTE data[64];
+    WORD datalen;
+    unsigned long long bitlen;
+    WORD state[4];
+} MD5_CTX;
+
+void md5_init(MD5_CTX *ctx);
+void md5_update(MD5_CTX *ctx, BYTE data[], size_t len); 
+void md5_final(MD5_CTX *ctx, BYTE hash[]);
+
+#endif // !MD5_H
+#define MD5_H
