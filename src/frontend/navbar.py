133bdad35ca4ac0e77756b7eea92c49ba6cfc4f4
phn210 <phamhonguyen210@gmail.com>
2025-02-03 15:16:46 +0100

    update ui logic

--- src/frontend/navbar.py
+++ src/frontend/navbar.py
@@ -8,27 +8,27 @@ from error_handling import show_error_dialog
 class NavBar(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super(NavBar, self).__init__(*args, **kwargs)
-        loadUi("ui_navbar.ui", self)
+        loadUi("wireframes/ui_navbar.ui", self)
         # self.setWindowTitle("Navbar")
-        # self.setButtonsHover()
+        self.setButtonsHover()
 
-#     def setButtonsHover(self):
-#         for button in self.findChildren(QtWidgets.QPushButton):
-#             button.setStyleSheet("""QPushButton
-# {
-#   	border-style: none;
-# 	color: white;
-# 	font-size: 20px;
-# 	font-weight: bold;
-# 	background-color: #3f4773;
-# 	border-radius: 10;
-# 	text-align: left;
-# 	padding-left: 10;
-# }
-# QPushButton:hover
-# {
-#   	background-color:#485187;
-# }""")
+    def setButtonsHover(self):
+        for button in self.findChildren(QtWidgets.QPushButton):
+            button.setStyleSheet("""QPushButton
+{
+  	border-style: none;
+	color: white;
+	font-size: 20px;
+	font-weight: bold;
+	background-color: #3f4773;
+	border-radius: 10;
+	text-align: left;
+	padding-left: 10;
+}
+QPushButton:hover
+{
+  	background-color:#485187;
+}""")
 
 if __name__ == "__main__":
     import sys
